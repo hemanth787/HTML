d83469ec8451cfd946c0ba97f9b74c8764c3a39a
Hemanth <kandurihemanth@gmail.com>
2021-06-18 12:00:13 +0530

    imageUrl fetching

--- data.js
+++ data.js
@@ -5,10 +5,16 @@ async function getData() {
     const {data} = await response.json();
     
     
-    const { likeCount, commentCount, unique_id } = data[0];
+    const { likeCount, commentCount, unique_id, image_url } = data[0];
     document.getElementById('like').textContent = likeCount;
     document.getElementById('comment').textContent = commentCount;
     document.getElementById('uniq').textContent = unique_id;
+    document.getElementById('img-prof').src = image_url;
+    // var img = document.createElement('img');
+    // img.src = "https://instagram.fhyd3-1.fna.fbcdn.net/v/t51.2885-15/s150x150/137032188_448911142974894_661426439725833092_n.jpg?tp=1&_nc_ht=instagram.fhyd3-1.fna.fbcdn.net&_nc_ohc=IJvBuqpKcE8AX_bO40A&edm=AGW0Xe4BAAAA&ccb=7-4&oh=636ee27e5d1731b73f480dfbd0f0935a&oe=60D28C99&_nc_sid=acd11b"
+    // document.getElementById('img-prof').appendChild('img');
+
+//  document.getElementById("img-prof").src="image_url";
 //    document.getElementById('img-prof').src = image_url;
    
     console.log(data);
